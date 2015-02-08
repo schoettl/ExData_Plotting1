3cc4f25d0e7fcefc739becc9cac9b79560c156df
Jakob Schöttl <jschoett@gmail.com>
2015-02-08 21:20:36 +0100

    Corrections

--- project1.R
+++ project1.R
@@ -2,11 +2,12 @@
 # awk 'NR==1; /^[12]\/2\/2007/' household_power_consumption.txt > household_power_consumption_subset.txt
 
 data = read.csv("household_power_consumption_subset.txt", sep=";", na.strings="?")
-data$datetime = strptime(paste(data$Date, data$Time), "%d/%m/%Y%H:%M:%S") # %Y is year with century -> no space necessary between date und time
+data$datetime = strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
 # Check data
 head(data)
-sapply(data, class)
+str(data)
 
 # Set locale to english
 Sys.setlocale("LC_TIME", "English")
+
