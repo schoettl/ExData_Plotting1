4b1a52ff96af94378dbeabf056de9d43f3e9181d
Jakob Schöttl <jschoett@gmail.com>
2015-02-12 22:23:38 +0100

    Even nicer Awk program :)

--- project1.R
+++ project1.R
@@ -1,5 +1,5 @@
 # Subset data with Awk in Bash:
-# awk 'NR==1; /^[12]\/2\/2007/' household_power_consumption.txt > household_power_consumption_subset.txt
+# awk 'NR==1 || /^[12]\/2\/2007/' household_power_consumption.txt > household_power_consumption_subset.txt
 
 data = read.csv("household_power_consumption_subset.txt", sep=";", na.strings="?")
 data$datetime = strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
